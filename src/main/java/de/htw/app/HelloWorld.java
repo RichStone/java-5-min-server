5a64613e6f1d2cd630217724e31ec708b136ce5a
RichStone <richard.had@hotmail.de>
2018-05-29 15:27:24 +0200

    add picture to web server

--- src/main/java/de/htw/app/HelloWorld.java
+++ src/main/java/de/htw/app/HelloWorld.java
@@ -4,9 +4,17 @@ import static spark.Spark.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-        get("/hello-print/*", (req, res) -> {
-        	return "hello-print";
+    	staticFiles.location("/public");
+    	
+        get("/", (request, response) -> "Hello World");
+        get("/hello-user/:name", (request, response) -> {
+        	String name = request.params(":name");
+        	return "hello-print " + name;
         });
+        get("/picture", (request, response) -> {
+        	return "<html><body><img src=imgs/sparkjava.png alt=\"Smiley face\"></body></html>";
+        });
+        // post("/user/new/:name", (...))
+        // request.params()
     }
 }
