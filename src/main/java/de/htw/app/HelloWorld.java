600a41129814ae8e2864e3f7969eb984f4e83f5a
RichStone <richard.had@hotmail.de>
2018-06-08 09:43:13 +0200

    whatever

--- src/main/java/de/htw/app/HelloWorld.java
+++ src/main/java/de/htw/app/HelloWorld.java
@@ -2,6 +2,7 @@ package de.htw.app;
 
 import static spark.Spark.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import spark.ModelAndView;
@@ -24,9 +25,23 @@ public class HelloWorld {
 //        	response.type("text/html,charset=utf-8");
         	return "<html><body><h1>Hello World ÜÄÖ</h1><img src=imgs/sparkjava.png alt=\"Smiley face\"></body></html>";
         });
+        get("/number", (request, response) -> {
+        	Map<String, Object> m = new HashMap<String, Object>();
+        	return render(m, "fibonacci.vm");
+        });
+        post("/number", (request, response) -> {
+        	Map<String, Object> m = new HashMap<String, Object>();
+        	String number = request.attribute("number");
+        	System.out.println(request.params());
+        	System.out.println(request.attributes());
+        	System.out.println(request.queryParams("number"));
+        	System.out.println(request.splat());
+        	return render(m, "fibonacci.vm");
+        });
     }
     
     public static String render(Map<String, Object> model, String templatePath) {
         return new VelocityTemplateEngine().render(new ModelAndView(model, templatePath));
     }
+    
 }
